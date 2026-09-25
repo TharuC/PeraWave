a348713a8ec6dd9f260f1ce78e5e6fb96a548b66
UdaraChandrasena <e23044@eng.pdn.ac.lk>
2026-09-25 19:46:41 +0530

    Delete test/test_database/post.sql

--- test/test_database/post.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public."PostVote" (
-    id integer NOT NULL,
-    "postId" integer NOT NULL,
-    "userId" integer NOT NULL,
-    value integer NOT NULL
-);
-
-ALTER TABLE ONLY public."PostVote"
-ADD CONSTRAINT "PostVote_pkey" PRIMARY KEY (id);
