0d5e160a8cab8ffd430e19b4f0210b249cd12eaa
UdaraChandrasena <e23044@eng.pdn.ac.lk>
2026-09-25 19:46:07 +0530

    Delete test/test_database/moderator_action.sql

--- test/test_database/moderator_action.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public."ModerationAction" (
-    id integer NOT NULL,
-    "moderatorId" integer,
-    "targetUserId" integer NOT NULL,
-    "actionType" text NOT NULL,
-    reason text,
-    "durationDays" integer,
-    "createdAt" timestamp(3) DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
